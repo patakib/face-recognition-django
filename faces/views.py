deb1caad6298e0ca6a9d38a621761149c5022def
patbalint <patakibalintka@gmail.com>
2020-11-01 11:10:31 +0100

    adding relative paths

--- faces/views.py
+++ faces/views.py
@@ -12,14 +12,14 @@ def image_view(request):
             img_obj = form.instance
 
             #magic
-            cv_img = cv.imread(r'e:\Dokumentumok\IT\Projects\cv\opencv\media\images\uploaded_img.jpg')
-            face_cascade = cv.CascadeClassifier('e:\Dokumentumok\IT\Projects\cv\cascade\haarcascade_frontalface_default.xml')
+            cv_img = cv.imread(r'media\images\uploaded_img.jpg')
+            face_cascade = cv.CascadeClassifier('cascade\haarcascade_frontalface_default.xml')
             frame_gray = cv.cvtColor(cv_img, cv.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(frame_gray, 1.3, 5)
             for (x,y,w,h) in faces:
                 center = (x + w//2, y + h//2)
                 cv.ellipse(cv_img, center, (w//2, h//2), 0, 0, 360, (100, 0, 0), 4)
-            cv.imwrite('e:\Dokumentumok\IT\Projects\cv\opencv\media\images\cvimage.jpg', cv_img)
+            cv.imwrite('media\images\cvimage.jpg', cv_img)
             #end of magic
 
             return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
